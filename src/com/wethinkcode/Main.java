d807f028a775951fce75ea09738b777f2c7714f6
martian1431 <pmalope@student.wethinkcode.co.za>
2020-07-17 10:03:50 +0200

    initial commit

---
+++ src/com/wethinkcode/Main.java
@@ -0,0 +1,9 @@
+package com.wethinkcode;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello avaj-launcher");
+    }
+}
