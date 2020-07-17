8846dcdc6246137690e32f1ee36a37a2e1d9d4f7
martian1431 <pmalope@student.wethinkcode.co.za>
2020-07-18 01:51:39 +0200

    added comments

--- src/com/wethinkcode/avaj/simulator/vehicles/Aircraft.java
+++ src/com/wethinkcode/avaj/simulator/vehicles/Aircraft.java
@@ -1,17 +1,37 @@
 package com.wethinkcode.avaj.simulator.vehicles;
 
+/**
+ * AirCraft
+ *
+ * @author  Phetho Malope
+ * @since   2020-10-20
+ * @version 1.0
+ */
 public class Aircraft {
     protected long id;
     protected String name;
     protected Coordinates coordinates;
     private static long _idCounter = 0L;
 
+    /**
+     * Aircraft constructor: when creating a
+     * new aircraft, the {@code name}
+     * is set to "name". {@code coordinates} and
+     * {@code id} are set "coordinates" & id respectively.
+     *
+     * @param name The name of the aircraft
+     * @param coordinates geolocation of the aircraft
+     */
     protected Aircraft(String name, Coordinates coordinates) {
         this.name = name;
         this.coordinates = coordinates;
         this.id = nextId();
     }
 
+    /**
+     * nextId: increment idCounter
+     * @return idCounter
+     */
     private long nextId() {
         return ++_idCounter;
     }
--- src/com/wethinkcode/avaj/simulator/vehicles/AircraftFactory.java
+++ src/com/wethinkcode/avaj/simulator/vehicles/AircraftFactory.java
@@ -2,9 +2,26 @@ package com.wethinkcode.avaj.simulator.vehicles;
 
 import static com.wethinkcode.avaj.simulator.utils.Const.*;
 
+/**
+ * AirCraftFactory
+ *
+ * @author  Phetho Malope
+ * @since   2020-10-20
+ * @version 1.0
+ */
 public class AircraftFactory {
+    /**
+     * Creates new aircraft, depending on the type {@code type} with
+     * the given {@code name}
+     *
+     * @param name Name of the aircraft.
+     * @param type Aircraft type.
+     * @param longitude Geolocation
+     * @param latitude Geolocation
+     * @param height Height
+     * @return the newly created aircraft.
+     */
     public static Flyable newAircraft(String type, String name, int longitude, int latitude, int height) {
-
         Coordinates coordinates = new Coordinates(longitude, latitude, height);
         switch (type.toLowerCase()){
             case HELICOPTER:
