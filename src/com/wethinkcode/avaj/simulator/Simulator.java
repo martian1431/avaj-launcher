4773e3de91e5a2787173405d3b2bf14994983356
martian1431 <pmalope@student.wethinkcode.co.za>
2020-08-13 18:47:04 +0200

    more validation

--- src/com/wethinkcode/avaj/simulator/Simulator.java
+++ src/com/wethinkcode/avaj/simulator/Simulator.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static com.wethinkcode.avaj.simulator.utils.Const.*;
+import static java.lang.Thread.sleep;
 
 public class Simulator {
     private static WeatherTower weatherTower;
@@ -19,27 +20,22 @@ public class Simulator {
 
     public static void main(String[] args) {
         try {
-            int simulations;
+//            int simulations;
             weatherTower = new WeatherTower();
             BufferedReader bufferedReader = new BufferedReader(new FileReader(args[0]));
             String line = bufferedReader.readLine();
+            int simulations = Integer.parseInt(line.split(SPACE)[SIMULATION_COUNT]);
 
-            if (line != null) {
-                simulations = Integer.parseInt(line.split(SPACE)[SIMULATION_COUNT]);
-
-                if (Integer.signum(simulations) != POSITIVE_NUMBER) {
-                    System.err.println(SIMULATION_COUNT_ERROR);
-                    System.exit(1);
-                }
-
-                createAircraft(bufferedReader);
-
-                registerAircraft(flyables);
-
-                startSimulation(simulations);
-            } else {
-                System.err.println("Scenario file: cannot be empty");
+            if (line.split(SPACE).length != POSITIVE_NUMBER) {
+                System.err.println(SCENARIO_FILE_INVALID_ARGUMENT);
                 System.exit(1);
+            } else if (Integer.signum(simulations) != POSITIVE_NUMBER) {
+                System.err.println(SIMULATION_COUNT_ERROR);
+                System.exit(1);
+            } else {
+                createAircraft(bufferedReader);
+                registerAircraft(flyables);
+                startSimulation(simulations);
             }
             bufferedReader.close();
 
@@ -50,32 +46,42 @@ public class Simulator {
             System.err.println(ERROR_READING_FILE + args[FILE]);
             System.exit(1);
         } catch (NumberFormatException e) {
-            System.err.println("Scenario file: invalid argument type");
+            System.err.println(SCENARIO_FILE_INVALID_ARGUMENT);
             System.exit(1);
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.err.println("Scenario file: index out of bounds");
+        } catch (ArrayIndexOutOfBoundsException | InterruptedException e) {
+            System.err.println(SCENARIO_FILE_INDEX_OUT_OF_BOUNDS);
+            System.exit(1);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
             System.exit(1);
         } finally {
             Log.logMessage();
-            System.out.println("Simulation completed...");
+            System.out.println(SIMULATION_COMPLETE);
         }
     }
 
-    private static void startSimulation(int simulations) {
+    private static void startSimulation(int simulations) throws InterruptedException {
         for (int index = 1; index <= simulations; index++) {
             weatherTower.changeWeather();
+            System.out.print("#");
+            sleep(300);
         }
+        System.out.println();
     }
 
-    private static void createAircraft(BufferedReader bufferedReader) throws IOException {
+    private static void createAircraft(BufferedReader bufferedReader) throws Exception {
         String line;
         while ((line = bufferedReader.readLine()) != null) {
-            Flyable aircraft = AircraftFactory.newAircraft(line.split(SPACE)[AIRCRAFT_TYPE],
-                    line.split(SPACE)[AIRCRAFT_NAME],
-                    Integer.parseInt(line.split(SPACE)[AIRCRAFT_LONGITUDE]),
-                    Integer.parseInt(line.split(SPACE)[AIRCRAFT_LATITUDE]),
-                    Integer.parseInt(line.split(SPACE)[AIRCRAFT_HEIGHT]));
-            flyables.add(aircraft);
+            if (line.split(SPACE).length != ARGUMENT_COUNT) { //TODO find a situable name for argument count
+                throw new Exception(SCENARIO_FILE_INVALID_ARGUMENT);
+            } else {
+                Flyable aircraft = AircraftFactory.newAircraft(line.split(SPACE)[AIRCRAFT_TYPE],
+                        line.split(SPACE)[AIRCRAFT_NAME],
+                        Integer.parseInt(line.split(SPACE)[AIRCRAFT_LONGITUDE]),
+                        Integer.parseInt(line.split(SPACE)[AIRCRAFT_LATITUDE]),
+                        Integer.parseInt(line.split(SPACE)[AIRCRAFT_HEIGHT]));
+                flyables.add(aircraft);
+            }
         }
     }
 
